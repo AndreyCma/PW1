3dacf0dbbab6fcd1d0f17cbad3f36ca6bddaf498
andreycm <andrey.chemyrtan@gmail.com>
2021-01-15 13:59:04 +0200

    Task 7

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    console.log(numbers);
-    numbers.splice(4,1,...[0,0,0]);
-
-    console.log(numbers);
+    console.log(extract(2,4));
+    
+    function extract(start,end) {
+        let extractedNumbers = new Array();
+        for (let i = start; i <= end; i++) {
+            extractedNumbers.push(numbers[i]);
+        }
+        return extractedNumbers;
+    }
       
       
 
