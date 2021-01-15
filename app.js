cf7c54f8b566c4d774dfcb81aa78d0d3447aef78
andreycm <andrey.chemyrtan@gmail.com>
2021-01-15 14:42:13 +0200

    Object Task 1

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    let numbers =  [8, 7,  2,  2,  3, 4, 2,  5, 5, 6, 7, 8, 5, 9, 0, 1];
-    console.log(uniqueSorted());
+    let user = new Object();
 
-    function uniqueSorted() {
-        let uniqueSet = new Set(numbers);
-        let uniqueArray = Array.from(uniqueSet);
-        let uniqueSortedArray = uniqueArray.sort();
-        return uniqueSortedArray;
-
-
+    user = {
+        name: "John",
+        surname: "Smith"
     }
+    console.log(user);
+
+    user.name = "Pete";
+    console.log(user);
+
+    delete user["name"];
+    console.log(user);
+
 
 
 });
