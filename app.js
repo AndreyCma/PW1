8f4306e44f376336e66795ee5695f6ddf58b3a9c
andreycm <andrey.chemyrtan@gmail.com>
2021-01-26 13:48:33 +0200

    Task 10.1

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  class Elem {
-    constructor(selector){
-      this.selector = selector;
-    }
+  const worker =
+  {
+    "Personal Information":{
+      "Full Name": "First Worker",
+      "Birthday":"30-09-1997",
+      "Mobile":"069876543",
+      "Home phone":"023139080",
+      "Address":"Balti,str.Decebal 1",
+      "gender":"male",
+      "IDNP":"1231124124",
+      "Place of Birth":"Balti",
+      "mail":"test@gmail.com"
+    },
+    "Work Information": {
+      "function":"developer",
+      "Department":"IT",
+      "Start Wokring Date":"20.08.2018",
+      "Work Expreience":3,
+      "Salary":10000,
+      "Graphic": {
+        "work":"8:00-17:00",
+        "lunch":"12:00-13:00"
+      }
+    }  
+  };
 
-    html(content) {
-      document.querySelector(this.selector).innerHTML += content;
-    }
+  let json = JSON.stringify(worker);
+  document.getElementById("JsonContent").innerHTML += json;
 
-    append(content) {
-      let existedContent = document.querySelector(this.selector).textContent;
-      document.querySelector(this.selector).innerHTML = existedContent + ' ' + content;
-    }
-    prepend(content) {
-      let existedContent = document.querySelector(this.selector).textContent;
-      document.querySelector(this.selector).innerHTML = content + ' ' + existedContent
-    }
 
-    attr(attr,value) {
-      document.querySelector(this.selector).setAttribute(attr,value);
-    }
-  }
-
-  let elem = new Elem('div');
-  elem.html("hello");
-  elem.append("content");
-  elem.prepend("begining");
-  elem.attr("class","www");
-  elem.attr("title","hello");
 
 
 
