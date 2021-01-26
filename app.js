78562716f58506244065d66fd518f0054c90d1df
andreycm <andrey.chemyrtan@gmail.com>
2021-01-26 12:43:03 +0200

    Task 6.2

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    function addTable() {
-        let DynamicTable = document.querySelector("#DynamicTable");
-      
-        let table = document.createElement('TABLE');
-        table.border = '1';
-      
-        let tableBody = document.createElement('TBODY');
-        table.appendChild(tableBody);
-      
-        for (let i = 0; i < 8; i++) {
-          let tr = document.createElement('TR');
-          tableBody.appendChild(tr);
-      
-          for (let j = 0; j < 8; j++) {
-            let td = document.createElement('TD');
-            td.width = '75';
-            td.height= '75';
-            if ((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1)){
-                td.style.background = "black";
-            }
-            tr.appendChild(td);
-          }
-        }
-        DynamicTable.appendChild(table);
-      }
-      addTable();
-    
+  class Person {
+    constructor(name,surname){
+      this.name = name;
+      this.surname = surname;
+    }
 
+    get getName(){
+      return this.name;
+    }
+    set setName(value){
+      this.name = value;
+    }
+    get getSurname(){
+      return this.surname;
+    }
+    set setSurname(value){
+      this.surname = value;
+    }
+
+  }
+
+  const worker = new Person('Иван', 'Иванов');
+  console.log(worker.getName);
+  console.log(worker.getSurname);
+  console.log(worker.setSurname = 'Петров');
+  console.log(worker.getSurname);
 
 });
